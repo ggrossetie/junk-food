152432a8ec3e769e1dbc7e3bd205d41de4dc2496
Guillaume Grossetie <g.grossetie@gmail.com>
2012-04-21 23:47:49 +0200

    Database config per environment

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,23 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "junk-food-#{Rails.env}"
+# config/initializers/mongo.rb
+
+db_config = YAML::load(File.read(File.join(Rails.root, "/config/mongo.yml")))
+
+if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
+  mongo = db_config[Rails.env]
+  host = mongo['host'] || 'localhost'
+  port = mongo['port'] || 27017
+  database = mongo['database']
+  uri = mongo['uri'] || 'mongodb://' + host + ':' + port + '/' + database
+  config = URI.parse(uri)
+
+  MongoMapper.connection = Mongo::Connection.new(config.host, config.port)
+  MongoMapper.database = config.path.gsub(/^\//, '')
+  MongoMapper.database.authenticate(config.user, config.password) if config.user
+end
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
      MongoMapper.connection.connect if forked
    end
 end
+ 
